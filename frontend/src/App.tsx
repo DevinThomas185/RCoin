0a230fcaef6e2c5cad7f2ded8a96144341eb9d96
Selih, Karim <karim.selih20@imperial.ac.uk>
2022-10-14 09:19:12 +0000

    Ks1020/setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import * as React from "react"
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from "@chakra-ui/react"
+import { ColorModeSwitcher } from "./ColorModeSwitcher"
+import Test from "./components/Test"
+import { Logo } from "./Logo"
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <Test></Test>
+      </Grid>
+    </Box>
+  </ChakraProvider>
+)
