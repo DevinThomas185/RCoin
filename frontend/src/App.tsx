42729d0ae1c4a19186e1c910e421665c824f3a9c
galenys <shivbhatia10@gmail.com>
2022-10-14 11:15:10 +0100

    add basic routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,24 @@
 import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import Test from "./components/Test"
-import { Logo } from "./Logo"
+import { BrowserRouter as Router, Link as RouterLink, Routes, Route } from "react-router-dom"
+import About from "./components/About"
+import Home from "./components/Home"
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <Test></Test>
-      </Grid>
-    </Box>
-  </ChakraProvider>
+  <Router>
+    <div className="App">
+      <ul className="App-header">
+        <li>
+          <RouterLink to="/">Home</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/about">About Us</RouterLink>
+        </li>
+      </ul>
+    </div>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/about' element={<About />}></Route>
+    </Routes>
+  </Router>
+
 )
---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,28 @@
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from "@chakra-ui/react"
+import { ColorModeSwitcher } from "../ColorModeSwitcher"
+import Test from "../components/Test"
+import { Logo } from "../Logo"
+
+let Home = () => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <Test></Test>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  )
+}
+
+export default Home;
