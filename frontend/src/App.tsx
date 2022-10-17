c050477c9c95cc3d2236d0bd9cd85ce07f99b5a9
galenys <shivbhatia10@gmail.com>
2022-10-17 14:01:42 +0100

    enforce authentication on routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { BrowserRouter as Router, Link as RouterLink, Routes, Route, useLocation, Navigate } from "react-router-dom";
 import Home from "./components/Home";
 
-const RequireAuth = (child: Element, isAuth: boolean) => {
+const RequireAuth = (child: JSX.Element, isAuth: boolean) => {
 
   if (!isAuth) {
     // Redirect them to the /login page, but save the current location they were
@@ -17,36 +17,53 @@ const RequireAuth = (child: Element, isAuth: boolean) => {
   return child;
 }
 
-
 const App = () => {
-  const [isAuth, setIsAuth] = useState(true);
+  const [isAuth, setIsAuth] = useState(false);
+  let header;
+  if (isAuth) {
+    header = (
+      <ul className="App-header">
+        <li>
+          <RouterLink to="/">Home</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/issue">Issue</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/trade">Trade</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/redeem">Redeem</RouterLink>
+        </li>
+      </ul>
+    )
+  } else {
+    header = (
+      <ul>
+        <li>
+          <RouterLink to="/">Home</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/sign-up">Sign up</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/login">Login</RouterLink>
+        </li>
+      </ul>
+    )
+  }
   return (
     <Router>
       <div className="App">
-        <ul className="App-header">
-          <li>
-            <RouterLink to="/">Home</RouterLink>
-          </li>
-          <li>
-            <RouterLink to="/login">Login</RouterLink>
-          </li>
-          <li>
-            <RouterLink to="/sign_up">Sign up</RouterLink>
-          </li>
-          <li>
-            <RouterLink to="/issue">Issue</RouterLink>
-          </li>
-          <li>
-            <RouterLink to="/trade">Trade</RouterLink>
-          </li>
-          <li>
-            <RouterLink to="/redeem">Redeem</RouterLink>
-          </li>
-        </ul>
+        {header}
       </div>
       <Routes>
         <Route path='/' element={<Home />}></Route>
-        <Route path='/about' element={<Home />}></Route>
+        <Route path='/issue' element={RequireAuth(<Home />, isAuth)}></Route>
+        <Route path='/trade' element={RequireAuth(<Home />, isAuth)}></Route>
+        <Route path='/redeem' element={RequireAuth(<Home />, isAuth)}></Route>
+        <Route path='/login' element={<Home />}></Route>
+        <Route path='/sign-up' element={<Home />}></Route>
       </Routes>
     </Router>
   )
