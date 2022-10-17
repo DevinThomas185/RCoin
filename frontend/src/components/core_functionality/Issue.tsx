80c0ffa67032287d4e98fb564f71b5a36fd51ecc
galenys <shivbhatia10@gmail.com>
2022-10-17 14:34:23 +0100

    add issue and trade components

---
+++ frontend/src/components/core_functionality/Issue.tsx
@@ -0,0 +1,7 @@
+const Issue = () => {
+  return (
+    <h1>Issuing new coins</h1>
+  )
+}
+
+export default Issue;
---
+++ frontend/src/components/core_functionality/Trade.tsx
@@ -0,0 +1,7 @@
+const Trade = () => {
+  return (
+    <h1>Issuing new coins</h1>
+  )
+}
+
+export default Trade;
