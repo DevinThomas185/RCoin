15c252ed87baafdb2d298a3f9d60998d87526b60
galenys <shivbhatia10@gmail.com>
2022-10-19 10:27:47 +0100

    add login and signup forms, which return JSON

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -8,7 +8,6 @@ import {
   Grid,
   theme,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "../ColorModeSwitcher"
 import Test from "../components/Test"
 
 const Home = () => {
@@ -16,7 +15,6 @@ const Home = () => {
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
           <Test></Test>
         </Grid>
       </Box>
