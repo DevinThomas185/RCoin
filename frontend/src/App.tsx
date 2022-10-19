8ce683bc399716163bcba1e688ed4e405721a797
Karim <selihkarim@gmail.com>
2022-10-19 18:55:17 +0100

    fixed test transaction promise issues

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,27 @@
-import * as React from "react"
+import * as React from "react";
 import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import Test from "./components/Test"
-import { Logo } from "./Logo"
-import PhantomSigner from "./components/phantom/Phantom"
+    ChakraProvider,
+    Box,
+    Text,
+    Link,
+    VStack,
+    Code,
+    Grid,
+    theme,
+} from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import Test from "./components/Test";
+import { Logo } from "./Logo";
+import { PhantomSigner } from "./components/phantom/Phantom";
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <Test></Test>
-        <PhantomSigner></PhantomSigner>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+    <ChakraProvider theme={theme}>
+        <Box textAlign="center" fontSize="xl">
+            <Grid minH="100vh" p={3}>
+                <ColorModeSwitcher justifySelf="flex-end" />
+                <Test></Test>
+                <PhantomSigner></PhantomSigner>
+            </Grid>
+        </Box>
+    </ChakraProvider>
+);
