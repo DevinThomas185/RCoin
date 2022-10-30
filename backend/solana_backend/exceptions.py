5af075e9909bccd50eef97644c779daabe1cd5bc
SzymonKubica <szymo.kubica@gmail.com>
2022-10-30 22:41:17 +0000

    Added custom exceptions to encapsulate different kinds of error behaviour.

--- backend/solana_backend/exceptions.py
+++ backend/solana_backend/exceptions.py
@@ -7,6 +7,15 @@ class NoTokenAccountException(Exception):
     def __str__(self):
         return "No token account was found for wallet {}".format(self.wallet_address)
 
+class TokenAccountAlreadyExists(Exception):
+    """Indicates that a token account associated with a given public key
+       already exists. Raised when we want to create a new token account."""
+    def __init__(self, wallet_address):
+        self.wallet_address = wallet_address
+
+    def __str__(self):
+        return "Token account already exists for wallet {}".format(self.wallet_address)
+
 class TransactionCreationFailedException(Exception):
     """Raised when construct_stablecoin_transaction function cannot
        successfully create a Transaction object"""
@@ -17,7 +26,18 @@ class TransactionCreationFailedException(Exception):
     def __str__(self):
         return "Unable to create transaction object because of \n {}".format(self.cause)
 
+class TransactionTimeoutException(Exception):
+    """Raised when an issue transaction takes too long to appear on the
+       blockchain"""
 
+    def __str__(self):
+        return "Transaction timed out."
+
+class BlockchainQueryFailedException(Exception):
+    """Raised when a query sent to the blockchain fails."""
+
+    def __str__(self):
+        return """Query sent to the blockchain was unsuccessful."""
 
 
 
