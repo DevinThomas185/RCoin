aa445ed4b944bba1d84bb08e7efa5f3ad51c6d14
Karim <selihkarim@gmail.com>
2022-11-02 16:09:51 +0000

    added json component from paystack fetch

--- frontend/src/components/Audit/CodeModal.tsx
+++ frontend/src/components/Audit/CodeModal.tsx
@@ -1,8 +1,12 @@
 import { Box, Button, Center, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Stack } from "@chakra-ui/react"
+import _ from "lodash";
+import { useState } from "react";
 import { CodeBlock } from "./CodeBlock"
+var JSONPretty = require('react-json-pretty');
 
 const CodeModal = ({isOpen, setOpen} : {isOpen: boolean, setOpen: any}) => {
-    let AuditCode = `import requests
+  // api key is hidden until we can get paystack to give us correct key
+  let AuditCode = `import requests
                     
 api_key = "empty"
 
@@ -12,36 +16,60 @@ response = requests.get(
 )
 
 print(response.json())`
-    
-    return (
-        <>
-          {/* <Button onClick={onOpen}>Open Modal</Button> */}
-    
-          <Modal isOpen={isOpen} size={'xl'} onClose={() => setOpen(false)}>
-            <ModalOverlay />
-            <ModalContent>
-              <ModalHeader>Verify Backing Balance</ModalHeader>
-              <ModalCloseButton />
-              <ModalBody>
-                <Center>
-                        <Stack>
-                            <Box>
-                                <CodeBlock code={AuditCode} language={'python'} showLineNumbers={true} />
-                            </Box>
-                            <Button>Fetch Balance</Button>
-                    </Stack>
-                </Center>
-              </ModalBody>
-    
-              <ModalFooter>
-                <Button colorScheme='blue' mr={3} onClick={() => setOpen(false)}>
-                  Close
-                </Button>
-              </ModalFooter>
-            </ModalContent>
-          </Modal>
-        </>
-      )
+
+  const [pasystackResponse, setPayStackResponse] = useState({});
+  
+  const handleFetchBalance = () => {
+    const requestOptions = {
+      method: "GET",
+      headers: { "Authorization": "Bearer empty" },
+    }
+    fetch("https://api.paystack.co/balance", requestOptions)
+      .then(res => res.json())
+      .then(data => {
+        setPayStackResponse(data);
+      })
+  }
+
+  const handleClose = () => {
+    setPayStackResponse({})
+    ;setOpen(false)
+  }
+
+  return (
+      <>
+        {/* <Button onClick={onOpen}>Open Modal</Button> */}
+  
+        <Modal isOpen={isOpen} size={'xl'} onClose={handleClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Verify Backing Balance</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Center>
+                      <Stack>
+                          {_.isEmpty(pasystackResponse) &&
+                            <>
+                              <CodeBlock code={AuditCode} language={'python'} showLineNumbers={true} />
+                              <Button onClick={handleFetchBalance}>Fetch Balance</Button>
+                            </>
+                          }
+                          {!_.isEmpty(pasystackResponse) &&
+                            <JSONPretty data={pasystackResponse} />
+                          }
+                      </Stack>
+              </Center>
+            </ModalBody>
+  
+            <ModalFooter>
+              <Button colorScheme='blue' mr={3} onClick={handleClose}>
+                Close
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
+    )
 }
 
 export default CodeModal
