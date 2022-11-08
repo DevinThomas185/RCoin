7c4a58d954f34ea41d3f1710702c78b9cdb09740
galenys <shivbhatia10@gmail.com>
2022-11-08 09:11:32 +0000

    refactor, add deposit and transfer screens

--- RCoin/App.tsx
+++ RCoin/App.tsx
@@ -1,54 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
+import HomeScreen from './Screens/HomeScreen';
+import DepositScreen from './Screens/DepositScreen';
+import TransferScreen from './Screens/TransferScreen';
 
 const Tab = createBottomTabNavigator();
 
-export default function App() {
+const App = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Deposit" component={DepositScreen} />
+        <Tab.Screen name="Transfer" component={TransferScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
