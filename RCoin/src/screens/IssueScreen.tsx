4307d5c7eff78613be967b444c5d5da22de1b08a
galenys <shivbhatia10@gmail.com>
2022-11-09 17:36:01 +0000

    add issue screen

---
+++ RCoin/src/screens/IssueScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const IssueScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Issue!</Text>
+    </View>
+  );
+};
+
+export default IssueScreen;
