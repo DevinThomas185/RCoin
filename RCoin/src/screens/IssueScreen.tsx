2defe8fb37893c0d3b68b50d395210235af1b968
galenys <shivbhatia10@gmail.com>
2022-11-10 10:00:05 +0000

    add basic three stages of issuing

--- RCoin/src/screens/IssueScreen.tsx
+++ RCoin/src/screens/IssueScreen.tsx
@@ -1,11 +1,18 @@
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import React from 'react';
-import { Text, View } from 'react-native';
+import MakingADeposit from './IssueStages/MakingADeposit'
+import PaymentSummary from './IssueStages/PaymentSummary'
+import PaymentConfirmation from './IssueStages/PaymentConfirmation'
+
+const Tab = createMaterialTopTabNavigator();
 
 const IssueScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Issue!</Text>
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Making A Deposit" component={MakingADeposit} />
+      <Tab.Screen name="Payment Summary" component={PaymentSummary} />
+      <Tab.Screen name="Payment Confirmation" component={PaymentConfirmation} />
+    </Tab.Navigator>
   );
 };
 
