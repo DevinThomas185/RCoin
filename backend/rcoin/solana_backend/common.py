0e34630eb9ce9bbd4d2532a7306d4b8cc1f5c759
SzymonKubica <szymo.kubica@gmail.com>
2022-12-01 21:55:15 +0000

    Addedd the deployment setup for the FEE_ACCOUNT

--- backend/rcoin/solana_backend/common.py
+++ backend/rcoin/solana_backend/common.py
@@ -9,6 +9,7 @@ from solana.rpc.api import Client
 
 SOLANA_CLIENT = Client(str(os.getenv("SOLANA_CLIENT")))
 MINT_ACCOUNT = PublicKey(str(os.getenv("MINT_ACCOUNT")))
+FEE_ACCOUNT = PublicKey(str(os.getenv("FEE_ACCOUNT")))
 TOKEN_OWNER = PublicKey(str(os.getenv("TOKEN_OWNER")))
 MULTISIG_ACCOUNT = PublicKey(str(os.getenv("MULTISIG_ACCOUNT")))
 RESERVE_ACCOUNT = PublicKey(str(os.getenv("RESERVE_ACCOUNT")))
@@ -24,6 +25,10 @@ def read_the_secret_key(key: str) -> list[int]:
 
 # Secret keys of the two signers that need to sign each blockchain transaction.
 SIGNER_1 = bytes(read_the_secret_key(str(os.getenv("SIGNER_1"))))
+
+# On the actual deployed backend the secret key of the second signer will not
+# be there. This one here is for the purposes of running the testsuite locally
+# as it is required to be able to manufacture both signatures while testing.
 SIGNER_2 = bytes(read_the_secret_key(str(os.getenv("SIGNER_2"))))
 
 # The precision that we support in transactions involving our stablecoin token
