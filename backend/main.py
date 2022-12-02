87f1ac9167757c33677b253b3b2a8f697f629d8b
Selih, Karim <karim.selih20@imperial.ac.uk>
2022-12-02 00:10:41 +0000

    Master fix

--- backend/main.py
+++ backend/main.py
@@ -20,12 +20,6 @@ import smtplib
 import ssl
 from email.message import EmailMessage
 
-import redis
-
-r = redis.StrictRedis(
-    host="localhost", port=6379, db=0, charset="utf-8", decode_responses=True
-)
-
 # import datetime
 from datetime import timezone, datetime, timedelta
 
