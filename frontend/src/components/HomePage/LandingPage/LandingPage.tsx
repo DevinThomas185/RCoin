a079bfaa3baf43a72e0cce53752dd2889e19e7d8
Szymon Kubica <szymo.kubica@gmail.com>
2022-12-21 20:17:39 +0100

    Added conditional rendering of the mockup phone depending on the size of the screen.

--- frontend/src/components/HomePage/LandingPage/LandingPage.tsx
+++ frontend/src/components/HomePage/LandingPage/LandingPage.tsx
@@ -1,15 +1,24 @@
-import { Box, Grid, HStack } from "@chakra-ui/react";
+import { Box, Grid, HStack, useBreakpointValue } from "@chakra-ui/react";
 import InformationPane from "../../Common/InformationPane";
 
 import LeftPane from "./LeftPane";
 import MockupPhone from "./MockupPhone";
 
 const LandingPage = ({ onLearnMore }: { onLearnMore: () => void }) => {
+  const showMockupPhone = useBreakpointValue(
+    {
+      base: false,
+      md: true,
+    },
+    {
+      fallback: "md",
+    }
+  );
   return (
     <InformationPane gradientDirection="to-br" gradientStrength={500}>
       <HStack justifyItems="center">
         <LeftPane onLearnMore={onLearnMore} />
-        <MockupPhone />
+        {showMockupPhone ? <MockupPhone /> : null}
       </HStack>
     </InformationPane>
   );
